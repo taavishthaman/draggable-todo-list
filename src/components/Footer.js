d03471fef87807bf6e73e03dc361b46a43d0498c
Taavish Thaman <taavishthaman97@gmail.com>
2023-09-12 15:20:06 +0530

    fix footer responsivenes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,12 +3,10 @@ import styles from "./Footer.module.css";
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <div>
-        <p className={styles.footerText}>Drag and drop to reorder list</p>
-        <p className={styles.footerText}>
-          Please wait for 1-2 minutes for server to boot up.
-        </p>
-      </div>
+      <p className={styles.footerText}>Drag and drop to reorder list</p>
+      <p className={styles.footerText}>
+        Please wait for 1-2 minutes for server to boot up.
+      </p>
     </footer>
   );
 }
