ea9f6c9f77e8c6781bcc9456e46adc4f26204f9e
Taavish Thaman <taavishthaman97@gmail.com>
2023-09-12 15:08:59 +0530

    add helpful footer text

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,12 @@ import styles from "./Footer.module.css";
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <p className={styles.footerText}>Drag and drop to reorder list</p>
+      <div>
+        <p className={styles.footerText}>Drag and drop to reorder list</p>
+        <p className={styles.footerText}>
+          Please wait for 1-2 minutes for server to boot up.
+        </p>
+      </div>
     </footer>
   );
 }
